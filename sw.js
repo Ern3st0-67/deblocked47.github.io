ce912438f72d0d9edd253759959afab0f9408c54
Ern3st0-67 <thiagocarrasco2014@gmail.com>
2026-04-18 00:15:08 -0500

    Update server.js

--- sw.js
+++ sw.js
@@ -1,19 +1,29 @@
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'START_SPAM') {
-    let count = 0;
-    const maxBurst = 65; 
-    
-    const spammer = setInterval(() => {
-      self.registration.showNotification("Your ChromeBook has 1000 viruses please go to sites.google.com/view/math-61/" + (count + 1), {
-        body: "ChromeOS Critical Failure: System integrity compromised. Action required.",
-        icon: "https://upload.wikimedia.org",
-        tag: "spam-" + Date.now() + "-" + count,
-        requireInteraction: true,
-        vibrate: [200, 100, 200]
-      });
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-      count++;
-      if (count >= maxBurst) clearInterval(spammer);
-    }, 150); 
-  }
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  
+  socket.on('user joined', (username) => {
+    socket.broadcast.emit('user joined', username);
+    console.log(`${username} connected`);
+  });
+
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
